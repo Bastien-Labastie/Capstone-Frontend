3633ff8adf14e9bc530826e08861a16f0d3d0ce4
Bastien-Labastie <98860146+Bastien-Labastie@users.noreply.github.com>
2025-02-26 21:21:27 -0500

    Update LoginForm.js

--- frontend/src/auth/LoginForm.js
+++ frontend/src/auth/LoginForm.js
@@ -25,7 +25,7 @@ function LoginForm({ login }) {
     evt.preventDefault();
     let result = await login(formData);
     if (result.success) {
-      history.push("/companies");
+      history.push("/spellList");
     } else {
       setFormErrors(result.errors);
     }
